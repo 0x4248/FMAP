7d7b4ef3dfc5c935bbf76d51853d6f695072b93b
lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>
2024-03-22 11:43:03 +0000

    Fix clicking issue
    Signed-off-by: lewisevans2007 <60709927+lewisevans2007@users.noreply.github.com>

--- FMAP/audio.py
+++ FMAP/audio.py
@@ -7,7 +7,21 @@
 import numpy as np
 import wave
 
-def render_audio(output, notes, note_duration, sample_rate):
+def apply_fade(signal, fade_in_samples, fade_out_samples):
+    signal = signal.astype(np.float64)
+
+    fade_in = np.linspace(0, 1, fade_in_samples)
+    fade_out = np.linspace(1, 0, fade_out_samples)
+
+    signal[:fade_in_samples] *= fade_in
+    signal[-fade_out_samples:] *= fade_out
+
+    signal = signal.astype(np.int16)
+    return signal
+
+
+
+def render_audio(output, notes, note_duration, sample_rate, fade_samples=1000):
     output_file = wave.open(output, 'w')
     output_file.setnchannels(1)
     output_file.setsampwidth(2)
@@ -19,6 +33,16 @@ def render_audio(output, notes, note_duration, sample_rate):
         t = np.linspace(0, note_duration, int(sample_rate * note_duration))
         note_wave = max_amplitude * np.sin(2 * np.pi * note * t)
         note_wave = note_wave.astype(np.int16)
+        try:
+            note_wave = apply_fade(note_wave, fade_samples, fade_samples)
+        except ValueError:
+            while True:
+                try:
+                    note_wave = apply_fade(note_wave, fade_samples, fade_samples)
+                    print(f"Fade samples reduced to {fade_samples}")
+                    break
+                except ValueError:
+                    fade_samples -= 1
         output_file.writeframes(note_wave.tobytes())
 
     output_file.close()
